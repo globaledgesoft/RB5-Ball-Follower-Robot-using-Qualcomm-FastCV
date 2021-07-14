a63d13680377bed53c9eceb7511851ed14cd5d15
Sahil Bandar <44898396+sahilbm@users.noreply.github.com>
2021-07-14 18:44:28 +0530

    Update build_shared_lib.py

--- build_shared_lib.py
+++ build_shared_lib.py
@@ -1,4 +1,4 @@
 import invoke
 invoke.run( "gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc -O3 -Wall -shared -fPIC src/ball_tracking.c "
-    "-o lib/libball_tracking.so -lpthread -lc -ldl -L. libfastcv.a "
+    "-o lib/libball_tracking.so -I. -lpthread -lc -ldl -L. libfastcv.a "
 )
